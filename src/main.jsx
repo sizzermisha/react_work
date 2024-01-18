38eaf856339dee40e2f6251e4dee1d295970da5f
sizzermisha <misha.cerez@gmail.com>
2024-01-18 13:10:52 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>  
+      <React.StrictMode>
+
+      </React.StrictMode>
+  </BrowserRouter>,
+)
